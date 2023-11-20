43847448024a41b881f76cc012f5f8f33251e3fd
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2023-11-20 08:57:58 -0500

    added hero image and blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,18 +1,38 @@
+import './BlogsPage.css'
+
 import { Container, Grid, GridCol } from '@mantine/core'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 import React from 'react'
 
 import BlogsList from '@/components/Blog/BlogsList'
+import getQueryClient from '@/react-query/getQueryClient'
+import BlogService from '@/services/blog.service'
+import { Blog } from '@/types/Blog'
+import { QueryParams } from '@/types/QueryParams'
 
-const BlogsPage: React.FC = () => {
+const BlogsPage: React.FC = async () => {
+  const queryClient = getQueryClient()
+  const blogService = new BlogService()
+
+  const params: QueryParams<Blog> = {
+    populate: ['mainImage', 'main_image_preview'],
+  }
+
+  await queryClient.prefetchQuery({
+    queryKey: BlogService.queryKeys.list(params),
+    queryFn: () => blogService.list(params),
+  })
+
   return (
-    <Container>
-      <Grid>
-        <GridCol span={12}>
-          <BlogsList />
-          <div className="blogs-background"></div>
-        </GridCol>
-      </Grid>
-    </Container>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Container>
+        <Grid>
+          <GridCol span={12}>
+            <BlogsList queryParams={params} />
+          </GridCol>
+        </Grid>
+      </Container>
+    </HydrationBoundary>
   )
 }
 
--- src/components/FooterCentered/index.tsx
+++ src/components/FooterCentered/index.tsx
@@ -1,20 +1,20 @@
 'use client'
-import React, { useState } from 'react'
-
-import { ActionIcon, Anchor, Button, Group, Image } from '@mantine/core'
-import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react'
 import './Footer.css'
+
+import { ActionIcon, Anchor, Group, Image } from '@mantine/core'
+import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react'
+import React from 'react'
 
 const links = [
   { link: '/support', label: 'Contact' },
-  { link: '#', label: 'Privacy' },
+  // { link: '#', label: 'Privacy' },
   { link: '/blogs', label: 'Blogs' },
   { link: '/products', label: 'Shop Products' },
   { link: '/about-us', label: 'About Us' },
 ]
 
 const FooterCentered = () => {
-  const [spinningButtonActionText, setSpinningButtonActionText] = useState('Stop')
+  // const [spinningButtonActionText, setSpinningButtonActionText] = useState('Stop')
   const items = links.map((link) => (
     <Anchor
       c="dimmed"
@@ -30,12 +30,33 @@ const FooterCentered = () => {
 
   return (
     <div className={'inner'}>
-      <div className="outer-hover-menu">
+      <Image className="footer-image" src={'/images/SeaTheMoss-StillSpinner.png'} />
+      <Group className={'links'}>{items}</Group>
+
+      <Group gap="xs" justify="flex-end" wrap="nowrap">
+        <ActionIcon size="lg" variant="default" radius="xl">
+          <IconBrandTwitter style={{ width: 18, height: 18 }} stroke={1.5} />
+        </ActionIcon>
+        <Anchor
+          c="dimmed"
+          href={'https://www.youtube.com/channel/UCdRAVki3AJLRUyi2sg-SliA'}
+          lh={1}
+          size="sm"
+          target="_blank"
+        >
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandYoutube style={{ width: 18, height: 18 }} stroke={1.5} />
+          </ActionIcon>
+        </Anchor>
+        <ActionIcon size="lg" variant="default" radius="xl">
+          <IconBrandInstagram style={{ width: 18, height: 18 }} stroke={1.5} />
+        </ActionIcon>
+      </Group>
+      {/* <div className="outer-hover-menu">
         <Image
           className={'footer-video'}
           src={'/videos/SeaTheMoss-Spinner-With-Wildcrafted-Raw-Extended.mp4'}
         />
-        <Image className="footer-image" src={'/images/SeaTheMoss-StillSpinner.png'} />
         <Button
           className="spinner-stopper-button"
           onClick={(e) => {
@@ -70,28 +91,7 @@ const FooterCentered = () => {
         >
           {spinningButtonActionText} Spinning Logo
         </Button>
-      </div>
-      <Group className={'links'}>{items}</Group>
-
-      <Group gap="xs" justify="flex-end" wrap="nowrap">
-        <ActionIcon size="lg" variant="default" radius="xl">
-          <IconBrandTwitter style={{ width: 18, height: 18 }} stroke={1.5} />
-        </ActionIcon>
-        <Anchor
-          c="dimmed"
-          href={'https://www.youtube.com/channel/UCdRAVki3AJLRUyi2sg-SliA'}
-          lh={1}
-          size="sm"
-          target="_blank"
-        >
-          <ActionIcon size="lg" variant="default" radius="xl">
-            <IconBrandYoutube style={{ width: 18, height: 18 }} stroke={1.5} />
-          </ActionIcon>
-        </Anchor>
-        <ActionIcon size="lg" variant="default" radius="xl">
-          <IconBrandInstagram style={{ width: 18, height: 18 }} stroke={1.5} />
-        </ActionIcon>
-      </Group>
+      </div> */}
     </div>
   )
 }
