6fc9d1f725964898441de93d874594fc933a3402
Hoan Than <hoanthan317@gmail.com>
2023-11-27 23:29:30 +0700

    debug: check image url

--- src/utils/cms.ts
+++ src/utils/cms.ts
@@ -1,5 +1,9 @@
 import { APP_CONFIG } from '@/config/app'
 
 export function getStrapiUploadUrl(url: string) {
+  console.log({
+    url,
+  })
+  if (APP_CONFIG.STRAPI.URL && url.startsWith(APP_CONFIG.STRAPI.URL)) return url
   return APP_CONFIG.STRAPI.URL + url
 }
