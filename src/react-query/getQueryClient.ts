0bca3c062faadb9d2a0337b0f093ce62c76fcd3f
Hoan Than <hoanthan317@gmail.com>
2023-11-30 20:48:20 +0700

    fix: try remove cache

--- src/react-query/getQueryClient.ts
+++ src/react-query/getQueryClient.ts
@@ -1,17 +1,13 @@
 import { QueryClient } from '@tanstack/react-query'
-import { cache } from 'react'
 
-// cache() is scoped per request, so we don't leak data between requests
-const getQueryClient = cache(
-  () =>
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 60 * 1000,
-          retry: false,
-          refetchOnWindowFocus: false,
-        },
+const getQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+        retry: false,
+        refetchOnWindowFocus: false,
       },
-    }),
-)
+    },
+  })
 export default getQueryClient
