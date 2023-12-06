a1aafb3a24ebdb2712ce9c5d3c8eb84946da59a7
Hoan Than <hoanthan317@gmail.com>
2023-12-06 21:45:34 +0700

    feat: add pagination to products page

--- src/app/products/ProductList.tsx
+++ src/app/products/ProductList.tsx
@@ -1,14 +1,18 @@
 'use client'
 
-import { Grid } from '@mantine/core'
+import { Grid, Pagination, Stack } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
-import React, { PropsWithChildren, useCallback, useState } from 'react'
+import Link from 'next/link'
+import { usePathname, useSearchParams } from 'next/navigation'
+import React, { PropsWithChildren, useCallback, useMemo, useState } from 'react'
 
 import ProductCard from '@/components/ProductCard'
 import ProductPreviewModal from '@/components/ProductPreviewModal'
 import { useProducts } from '@/queries/useProducts'
 import { Product, Product_Plain } from '@/types/Product'
 import { QueryParams } from '@/types/QueryParams'
+
+import { pagination } from './ProductList.css'
 
 const ProductCol: React.FC<PropsWithChildren> = ({ children }) => {
   return (
@@ -29,6 +33,9 @@ export type ProductListProps = {
 }
 
 const ProductList: React.FC<ProductListProps> = ({ queryParams }) => {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null)
   const [productPreviewOpened, productPreview] = useDisclosure(false, {
     onClose: () => {
@@ -38,6 +45,12 @@ const ProductList: React.FC<ProductListProps> = ({ queryParams }) => {
 
   const { data: products } = useProducts(queryParams)
 
+  const totalPages = useMemo(
+    () =>
+      Math.ceil((products?.meta.pagination.total ?? 0) / (queryParams.pagination?.pageSize || 1)),
+    [products?.meta.pagination.total, queryParams.pagination?.pageSize],
+  )
+
   const onAddToCart = useCallback(
     async (product: Product) => {
       setSelectedProduct(product)
@@ -46,8 +59,15 @@ const ProductList: React.FC<ProductListProps> = ({ queryParams }) => {
     [productPreview],
   )
 
+  const buildPageHref = (page: number) => {
+    const newSearchParams = new URLSearchParams(searchParams)
+    newSearchParams.set('page', page.toString())
+    const href = `${pathname}?${newSearchParams.toString()}`
+    return href
+  }
+
   return (
-    <>
+    <Stack gap="lg">
       <Grid>
         {products?.data?.map((product) => (
           <ProductCol key={product.id}>
@@ -55,12 +75,56 @@ const ProductList: React.FC<ProductListProps> = ({ queryParams }) => {
           </ProductCol>
         ))}
       </Grid>
+      <Pagination
+        className={pagination}
+        total={totalPages}
+        value={queryParams.pagination?.page}
+        getItemProps={(page) => ({
+          component: Link,
+          href: buildPageHref(page),
+        })}
+        getControlProps={(control) => {
+          if (control === 'next') {
+            const disabled = queryParams.pagination?.page === totalPages
+            return {
+              component: Link,
+              href: buildPageHref((queryParams.pagination?.page ?? 0) + 1),
+              disabled,
+              ...(disabled
+                ? {
+                    style: {
+                      pointerEvents: 'none',
+                    },
+                  }
+                : {}),
+            }
+          }
+
+          if (control === 'previous') {
+            const disabled = (queryParams.pagination?.page || 1) === 1
+            return {
+              component: Link,
+              href: buildPageHref((queryParams.pagination?.page ?? 2) - 1),
+              disabled,
+              ...(disabled
+                ? {
+                    style: {
+                      pointerEvents: 'none',
+                    },
+                  }
+                : {}),
+            }
+          }
+
+          return {}
+        }}
+      />
       <ProductPreviewModal
         opened={productPreviewOpened}
         onClose={productPreview.close}
         product={selectedProduct}
       />
-    </>
+    </Stack>
   )
 }
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,10 @@
 import { Container, Grid, GridCol } from '@mantine/core'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
+import { ROUTE_PATHS } from '@/consts/route-paths'
 import getQueryClient from '@/react-query/getQueryClient'
 import ProductService from '@/services/product.service'
 import { Product_Plain } from '@/types/Product'
@@ -14,13 +16,30 @@ export const metadata: Metadata = {
   title: 'Products | SeaTheMoss',
 }
 
-// TODO: Add pagination
-const ProductsPage: React.FC = async () => {
+type ProductsPageProps = {
+  searchParams: {
+    page?: string
+  }
+}
+
+const LIMIT = 12
+
+const ProductsPage: React.FC<ProductsPageProps> = async ({ searchParams }) => {
   const queryClient = getQueryClient()
   const productService = new ProductService()
 
+  const page = parseInt(searchParams.page || '1') // default to page 1
+
+  if (isNaN(page) || page < 1) {
+    redirect(ROUTE_PATHS.PRODUCT.INDEX)
+  }
+
   const params: QueryParams<Product_Plain> = {
     populate: ['images', 'thumbnail', 'product_variants'],
+    pagination: {
+      page,
+      pageSize: LIMIT,
+    },
   }
 
   await queryClient.prefetchQuery({
