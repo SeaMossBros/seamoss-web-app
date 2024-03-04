fa79ae6aa2a81cac653b3c0eef2c21cafecfa2a4
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-03-03 19:03:49 -0500

    fixed lint error

--- src/app/auth/google/callback/CallBackClientSide.tsx
+++ src/app/auth/google/callback/CallBackClientSide.tsx
@@ -1,12 +1,8 @@
 'use client'
 
 import { Center, Stack } from '@mantine/core'
-import { useSearchParams } from 'next/navigation'
 
 const CallBackClientSide: React.FC = () => {
-  const searchParams = useSearchParams()
-  // console.log('search params from callback page', searchParams)
-
   return (
     <Stack justify="center" mt={64}>
       <Center>
